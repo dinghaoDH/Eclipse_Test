ca552d5d459c3c056341ecd544893041b66faa61
dinghao <1239509851@qq.com>
2015-06-02 18:12:13 +0800

    3

--- src/aaa.java
+++ src/aaa.java
@@ -1,5 +1,16 @@
 import java.lang.*;
+import java.util.*;
+
 public class aaa 
 {
-  private int a;
+	private int a;
+	private int b;
+    private int c;
+    private int d;
+    
+	public aaa()
+	{
+	
+	}
+	
 }
